b76fbca910c046b61926350ef8cd56f3002bf783
Moha-001-commit <mohamedheshamm@aucegypt.edu>
2020-09-08 11:27:18 +0200

    H

--- Source.cpp
+++ Source.cpp
@@ -13,4 +13,15 @@ int knapSack(int W, int wt[], int val[], int n)
 		else
 			return max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt,
 				val, n - 1));
+}// Driver code
+int main()
+{
+	int val[] = { 60, 100, 120 };
+	int wt[] = { 10, 20, 30 };
+	int W = 50;
+	int n = sizeof(val) / sizeof(val[0]);
+	cout << knapSack(W, wt, val, n);
+	cout << "edited by H";
+	return 0;
 }
+// This code is contributed by rathbhupendra
